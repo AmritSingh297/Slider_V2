5f5f85220c3b7bed7008942070c75d3b18ed446c
AmritSingh297 <moneyji29700@gmail.com>
2019-11-05 15:28:37 +0530

    slider with jquery animate

--- Testimonial 2/function.js
+++ Testimonial 2/function.js
@@ -1,30 +1,30 @@
 var slideIndex = 1;
 var val;
 showDivs(slideIndex);
-
 function plusDivs(n) {
-  showDivs(slideIndex += n); 
-  val.classList.remove('right-animate');
-  val.classList.add("left-animate");
+  $('.mySlides').animate({ marginLeft: '-966px' }, 1000);
+  setTimeout(function () {
+    showDivs(slideIndex += n);
+  }, 1025);
 }
 function minusDivs(n) {
-  showDivs(slideIndex += n);
-  val.classList.remove('left-animate');
-  val.classList.add("right-animate");
+  $('.mySlides').animate({ marginLeft: '966px' }, 1000);
+  setTimeout(function () {
+    showDivs(slideIndex += n);
+  }, 1025);
 }
 
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
+  if (n > x.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = x.length }
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
-  val =  x[slideIndex-1];
+  val = x[slideIndex - 1];
   animation(val);
-  function animation(val){
-    //val.style.display = 'flex';
+  function animation(val) {
     val.setAttribute(
       "style", "display: flex;");
 
